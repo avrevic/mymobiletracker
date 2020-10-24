3cbfdc2ce171f11fd3c9972a510de3e136f5fdeb
taki <takitaki228@gmail.com>
2020-10-24 23:38:37 +0900

    Add CreateAPIKey and Collect_Device_Info functions
    1) This is just a general overview of functions needed for mobile app install.

---
+++ collect_user_info.php
@@ -0,0 +1,8 @@
+<?php
+    function Collect_Device_Info()
+    {
+        echo "This function collects unique features from user's device (OS, Processor, Memory, Supported Network, etc)";
+    }
+
+    function 
+?>
