a60fa8a32913fafc3e99ea98d11eb6944ffd8943
Taki431 <takitaki228@gmail.com>
2020-10-25 19:38:58 +0000

    Add pseudocode + description for app install endpoint

--- php/apps/install.php
+++ php/apps/install.php
@@ -1,9 +1,60 @@
 <?php
 
+/**
+ * Short description for class
+ *
+ * Long description for class (if any)...
+ *
+ */ 
+
 require_once('../Util/bootstrap.php');
 
 header('Content-Type: application/json');
 $data = array("Test" => "Armin");
 echo json_encode($data);
+
+/*
+Description: This endpoint is called when the user installs the app on their device. 
+There are 5 extra parameters that can be used for some extra information about the app that we want to collect. 
+They are optional. Please do some research and pick max 5 parameters that are common for android/ios devices - like device type, model etc. 
+Token parameters is used so that only authorized users can call our service
+
+Method: POST 
+
+URL: /apps/install
+
+Parameters:
+
+{
+	“app_id”: “com.myappid.app”,
+	// device info
+	“device”: “iphone8”
+	… send as much as you can depending on the device
+	“Operating Systemparam1”: “”,
+	“Processorparam2”: “”,
+	“Memoryparam3”: “”,
+	“Supported Networkparam4”: “”,
+	“param5”: “”,
+	“token”: “12345”
+}
+
+Response:
+
+HTTP 200 for successful user login. 
+
+*/
+
+
+
+/*
+******PSEUDOCODE******
+
+1) Get all params and store them into variables
+2) Validate them 
+3) Check if the app exists in the system
+4) Store the user data in the database
+5) Return user ID back to the app
+
+*/
 
 ?>
