a8a61ab60e9ff8722c55c9dfbfec61f439e825ea
Armin Vrević <armin.vrevic@gmail.com>
2020-10-25 20:28:04 +0100

    Reorganize project into diff folders

---
+++ php/Util/db.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Short description for class
+ *
+ * Long description for class (if any)...
+ *
+ */ 
+
+/**
+ * Returns database handler that can be used to execute queries
+ */
+function dbConnect()
+{
+}
+
+/**
+ * disconnects active db session 
+ */
+function dbDisconnect()
+{
+}
+
+?>
---
+++ php/Util/util.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Short description for file
+ *
+ * Long description for file (if any)...
+ *
+ */ 
+
+/**
+ * Returns database handler that can be used to execute queries
+ * 
+ * @param ip Ip for which we want to get the country
+ * @return country Country for the requested IP
+ */
+function getCountry($ip)
+{
+}
+
+/**
+ * disconnects active db session 
+ */
+function dbDisconnect()
+{
+}
+
+?>
