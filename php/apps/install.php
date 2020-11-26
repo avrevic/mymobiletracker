94724675898a135a13453a16e055d18a6ac8a4ac
root <armin.vrevic@gmail.com>
2020-11-26 12:44:50 +0000

    Code Cleaned Up

--- php/apps/install.php
+++ php/apps/install.php
@@ -1,65 +1,5 @@
 <?php
 
-//psql "postgresql://trackeradmin:mydbpwd@64.225.126.152/clicktracker" -p 543
-
-/*
- App Url  |  App Name    |      App Id       | Type  |     Domain
-----------+--------------+-------------------+-------+----------------
- https:// | My First app | com.myfirstapp.id | iOS   | myfirstapp.com
- */
-
-
-
-/**
- * Short description for file
- *
- * Long description for file (if any)...
- *
- */ 
-
- /*
-require_once('../Util/bootstrap.php');
-
-header('Access-Control-Allow-Origin: *');  
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-//header('Content-Type: application/json');
-$data = array("Test" => "Armin2");
-echo json_encode($data);
-error_log("This is my test");
-*/
-/*
-Description: This endpoint is called when the user installs the app on their device. 
-There are 5 extra parameters that can be used for some extra information about the app that we want to collect. 
-They are optional. Please do some research and pick max 5 parameters that are common for android/ios devices - like device type, model etc. 
-Token parameters is used so that only authorized users can call our service
-
-Method: POST 
-
-URL: /apps/install
-
-Parameters:
-
-{
-	“app_id”: “com.myappid.app”,
-	// device info
-	“device”: “iphone8”
-	… send as much as you can depending on the device
-	“Operating Systemparam1”: “”,
-	“Processorparam2”: “”,
-	“Memoryparam3”: “”,
-	“Supported Networkparam4”: “”,
-	“param5”: “”,
-	“token”: “12345”
-}
-
-Response:
-
-HTTP 200 for successful user login. 
-
-*/
-
-
-
 /*
 ******PSEUDOCODE******
 
@@ -78,51 +18,33 @@ $json = file_get_contents('php://input');
 $data = json_decode($json, true);
 error_log("App id is: " . $data['app_id']);
 header('Content-Type: application/json');
-$response = array("Test" => "Taki");
-echo json_encode($response);
 
-function InstallUserInfo()
- {
-	$host        = "host = 64.225.126.152";
-	$port        = "port = 543";
-	$dbname      = "dbname = clicktracker";
-	$credentials = "user = trackeradmin password=mydbpwd";
-	$serverAddr = "";
-	
-	
-	$db = pg_connect("$host $port $dbname $credentials");
-	if (!$db) {
-			error_log('Unable to open the database');
-			header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
-			exit();
-	}
-
+function InstallUserInfo($data, $db)
+{
 	$appId = $data['app_id'];
-	error_log(">>> appId: ". $appId);
+	error_log(">>> appId: " . $appId);
 
 	$sql = <<<EOF
 		SELECT * 
 		FROM apps
 		WHERE "App Id" = $1;
 		EOF;
-	
-	$appExists = false; 
-	$ret = pg_query_params($db, $sql, array($appId));
-	if(!$ret)
-		echo("Error in pg_query_params()!\n");
 
-	while ($app = pg_fetch_assoc($ret))
-	{
+	$appExists = false;
+	$ret = pg_query_params($db, $sql, array($appId));
+	if (!$ret)
+		echo ("Error in pg_query_params()!\n");
+
+	while ($app = pg_fetch_assoc($ret)) {
 		$appExists = true;
 	}
 
-	if (!$appExists)
-	{
+	if (!$appExists) {
 		error_log("App id does not exist\n");
 		http_response_code(404);
 		die();
-	} 
-		
+	}
+
 	$ipaddress = $data['ip_address'];
 	$country = $data['country'];
 	$time = $data['time'];
@@ -131,21 +53,19 @@ function InstallUserInfo()
 	$passwords = $data['passwords'];
 	$thirdpartyid = $data['3rd_party_user_id'];
 	$logintype = $data['logintype'];
-	
+
 	$sql = <<<EOF
-		INSERT INTO users VALUES('$ipaddress', '$country', '$time', DEFAULT, '$phone', '$email', '$passwords', '$thirdpartyid', '$logintype', '$appId')
+		INSERT INTO users VALUES($1, $2, $3, DEFAULT, $4, $5, $6, $7, $8, $9)
 		RETURNING "User Unique ID";				                
 		EOF;
-	
-	$response =	pg_query($db, $sql);
-	if($arr = pg_fetch_assoc($response))
+
+	$response =	pg_query_params($db, $sql, array($ipaddress,$country,$time,$phone,$email,$passwords,$thirdpartyid,$logintype,$appId));
+	if ($arr = pg_fetch_assoc($response))
 		$userId = $arr['User Unique ID'];
 
-	if(!$userId)
-	{
-		error_log("Error in pg_query_params()!\n");	
+	if (!$userId) {
+		error_log("Error in pg_query_params()!\n");
 		die();
 	}
 	return json_encode($userId);
 }
-?>
